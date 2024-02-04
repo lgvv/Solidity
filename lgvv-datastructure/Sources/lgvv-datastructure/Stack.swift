ca720070c4ee1ce65ad7f2b6cc95169d4844e375
GeonWoo <lgw9898@naver.com>
2024-02-04 20:31:21 +0900

    스택 자료구조를 구현

--- lgvv-datastructure/Sources/lgvv-datastructure/Stack.swift
+++ lgvv-datastructure/Sources/lgvv-datastructure/Stack.swift
@@ -1,2 +1,38 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Stack.swift
+//
+//
+//  Created by Geon Woo lee on 2/4/24.
+//
+
+public struct Stack<T> {
+    /// 요소
+    private var elements: [T] = []
+    
+    private init() {}
+    
+    /// 스택에 삽입
+    public mutating func push(element: T) { 
+        elements.append(element)
+    }
+    
+    /// 스택에서 요소를 꺼냄
+    public mutating func pop(element: T) -> T? {
+        elements.popLast()
+    }
+    
+    /// 스택에서 요소를 확인
+    public mutating func peek() -> T? {
+        elements.last
+    }
+    
+    /// 스택이 비어있는지 확인
+    public var isEmpty: Bool {
+        elements.isEmpty
+    }
+    
+    /// 스택에 들어있는 개수
+    public var count: Int {
+        elements.count
+    }
+}
